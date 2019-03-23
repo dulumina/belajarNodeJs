5ae87fde87093ed27d50a0864cc3180869758712
dzulfikri <fikri.dulumina@gmail.com>
2019-03-23 19:04:25 +0800

    templating
    belajar routing dantemplating menggunakan hbs

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-var express = require('express');
-var app = express();
+const path 		= require('path')
+const express 	= require('express')
+const app 		= express()
 
-app.get('/',function(req,res){
-	res.send('<h1>hallo semuanya...!</h1>');
+
+// View engine setup
+app.set('views', path.join(__dirname, 'templates'));
+app.set('view engine', 'hbs');
+
+
+// Routing
+app.get('/',( req, res) => {
+	res.render('index',{
+		nama : 'Dzulfikri'
+	})
 });
 
+
+// Listen port
 app.listen(3000);
+console.log('server sedang berjalan di Port 3000, http://localhost:3000')
