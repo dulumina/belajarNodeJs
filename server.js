9a3b7c896d7a136e2fbfb07071fac343dc6725ee
dzulfikri <fikri.dulumina@gmail.com>
2019-03-23 19:30:49 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app 		= express()
 // View engine setup
 app.set('views', path.join(__dirname, 'templates'));
 app.set('view engine', 'hbs');
-
+app.use(express.static('asset')); // menentukan lokasi statis file seperti css, js,dan lain2.
 
 // Routing
 app.get('/',( req, res) => {
